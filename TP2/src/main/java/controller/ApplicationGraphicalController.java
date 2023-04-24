5e4ab05911e7a0151772ef895838d037ab615f04
elie987654321 <elie987654321@gmail.com>
2023-04-24 13:49:55 -0400

    continuer graphical controller

--- TP2/src/main/java/controller/ApplicationGraphicalController.java
+++ TP2/src/main/java/controller/ApplicationGraphicalController.java
@@ -2,42 +2,80 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import model.*;
+import org.w3c.dom.Text;
+
 public class ApplicationGraphicalController {
     @FXML
     private TextField nomAcheteur;
-
-    private String nomAcheteurString;
-
     @FXML
     private ChoiceBox modePaiement;
-
-
-
     @FXML
     private TextField totalSansTaxe;
-
-    private double totalSansTaxeDouble;
-
     @FXML
     private TextField taxesApplicables;
-
-    private double taxesApplicablesDouble;
-
     @FXML
     private TextField totalAvecTaxes;
 
-    private double totalAvecTaxesDouble;
-
-    private Facture facture;
+    @FXML
+    private Button ajouter;
 
     private Comptable comptable;
 
-
-
     private void HandleBoutonClickAjouter(ActionEvent eventHandler)
+    {
+        Facture factureAEntrer = new Facture();
+        if(ChaineValide(totalSansTaxe.getText()) && ChaineValide(totalAvecTaxes.getText()) && ChaineValide(totalAvecTaxes.getText()))
+        {
+
+        }
+  }
+
+  public boolean ChaineValide(String chaine)
+  {
+      if(chaine == "" || chaine == null)
+      {
+          return false;
+      }
+
+      boolean charInvalide = false;
+      char[] characteres = chaine.toCharArray();
+      int i = 0;
+
+      while(charInvalide == false && i < characteres.length)
+      {
+            if(
+                    !(
+                        characteres[i] == '0'  ||
+                        characteres[i] == '1'  ||
+                        characteres[i] == '2'  ||
+                        characteres[i] == '3'  ||
+                        characteres[i] == '4'  ||
+                        characteres[i] == '5'  ||
+                        characteres[i] == '6'  ||
+                        characteres[i] == '7'  ||
+                        characteres[i] == '8'  ||
+                        characteres[i] == '9'
+                    )
+            )
+            {
+                charInvalide = true;
+            }
+          i++;
+      }
+
+      if(charInvalide == true)
+      {
+          return false;
+      }
+      else
+      {
+          return true;
+      }
+  }
+
+    public int ApplicationController()
     {
 
     }
@@ -49,6 +87,10 @@ public class ApplicationGraphicalController {
 
     public void Initialize()
     {
-        Comptable comptable = new Comptable();
+        comptable = new Comptable();
+
+        ajouter.setOnAction(this::HandleBoutonClickAjouter);
+
+
     }
 }
