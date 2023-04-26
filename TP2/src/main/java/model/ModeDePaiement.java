1f393a04d356fae91ecf19d7fb15ea50a695c346
AC-climoilou <1701633@cegeplimoilou.ca>
2023-04-26 08:53:32 -0400

    Factory fini

--- TP2/src/main/java/model/ModeDePaiement.java
+++ TP2/src/main/java/model/ModeDePaiement.java
@@ -1,34 +1,12 @@
 package model;
 
-public class ModeDePaiement{
+public interface ModeDePaiement{
 
     // Attributs
     public static final double POURCENTAGE = 0.02;
-    private String nom;
-    private double frais;
-
-    // Constructeur avec paramètres
-    public ModeDePaiement(String nom, double frais)
-    {
-        this.nom = nom;
-        this.frais = frais;
-    }
 
     // Accesseurs et mutateurs
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public double getFrais() {
-        return frais;
-    }
-
-    public void setFrais(double frais) {
-        this.frais = frais;
-    }
+    public double getFrais();
+    public void setFrais(double frais);
 
 }
